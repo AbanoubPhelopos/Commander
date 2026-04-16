ed7d4acefa025bdcd17dd64f82fe613a2e557947
abanoub saweris <abanoubphelopos12@gmail.com>
2026-04-16 20:01:22 +0200

    Simplifies DB context constructor

--- src/api/commander.infrastructure/Persistence/AppDbContext.cs
+++ src/api/commander.infrastructure/Persistence/AppDbContext.cs
@@ -8,10 +8,7 @@ using Microsoft.EntityFrameworkCore;
 namespace commander.infrastructure.Persistence;
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-        
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Platform> Platforms { get; set; }
 }
