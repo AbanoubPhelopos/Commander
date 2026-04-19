6934617e8053f0838e9e4fe928e0b6bcc1d9cc81
abanoub saweris <abanoubphelopos12@gmail.com>
2026-04-19 20:29:43 +0200

    Feat: Register Command dependencies

--- src/api/commander.api/Registrations/Dependencies.cs
+++ src/api/commander.api/Registrations/Dependencies.cs
@@ -1,5 +1,7 @@
+using commander.application.Features.Commands.Mappings;
 using commander.application.Features.Platforms.Commands.Create;
 using commander.application.Features.Platforms.Commands.Update;
+using commander.application.Features.Platforms.Mappings;
 using commander.domain.Interfaces;
 using commander.infrastructure.Persistence;
 using commander.infrastructure.Persistence.Repositories;
@@ -21,10 +23,17 @@ internal static class Dependencies
             options.UseNpgsql(connectionString));
 
         services.AddScoped<IPlatformRepository, PlatformRepository>();
+        services.AddScoped<ICommandRepository, CommandRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         services.AddValidatorsFromAssemblyContaining<CreatePlatformCommandValidator>();
         TypeAdapterConfig.GlobalSettings.Default.NameMatchingStrategy(NameMatchingStrategy.IgnoreCase);
+
+        CommandMappingProfile commandProfile = new();
+        commandProfile.Register(TypeAdapterConfig.GlobalSettings);
+
+        PlatformMappingProfile platformProfile = new();
+        platformProfile.Register(TypeAdapterConfig.GlobalSettings);
 
         return services;
     }
