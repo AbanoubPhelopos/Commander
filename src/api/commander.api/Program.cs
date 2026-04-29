cbb78a7328462fc5954fe6860b339cd55b37db29
abanoub saweris <abanoubphelopos12@gmail.com>
2026-04-29 16:07:59 +0300

    Integrate Serilog for structured logging

--- src/api/commander.api/Program.cs
+++ src/api/commander.api/Program.cs
@@ -2,9 +2,13 @@ using Commander.Api.Registrations;
 using commander.application.Features.Platforms.Commands.Create;
 using commander.application.Features.Platforms.Queries.GetAll;
 using commander.api.Middleware;
+using Serilog;
 
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
@@ -19,12 +23,14 @@ builder.Services.AddDependencies(builder.Configuration);
 
 WebApplication app = builder.Build();
 
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
